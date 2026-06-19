b0915d67109bc07455e51a2916f271c31fa601a2
Oleg Smacinih <olegsmacinih@coherentsolutions.com>
2026-06-19 12:01:53 +0300

    Exception handling middleware

---
+++ Events.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace Events.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Unhandled exception");
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                message = "Unexpected error occured"
+            };
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
--- Events.Api/Program.cs
+++ Events.Api/Program.cs
@@ -1,5 +1,9 @@
 using Events.Api.Extensions;
+using Events.Api.Middlewares;
 using Events.Api.Services;
+using Events.Contracts.Abstractions;
+using Events.Infrastructure;
+using Events.Infrastructure.DynamoDb;
 using Events.Infrastructure.RabbitMq;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +12,13 @@ builder.Services.AddControllers();
 builder.Services.AddSwagger();
 
 builder.Services.AddRabbitMqPublishInfrastructure(builder.Configuration);
-builder.Services.AddScoped<EventService>();
+builder.Services.AddDynamoDbInfrastructure(builder.Configuration);
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventService, EventService>();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseSwagger();
 app.UseSwaggerUI();
